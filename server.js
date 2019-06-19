ce83d0329f39b72be295ef4fd197724915ca71bf
lowlyHero <e.nunn1009@gmail.com>
2019-06-19 12:46:07 -0400

    Pushing commits for changes made before first break

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
+const logger = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+//Global middleware
+server.use(logger('dev'));
 server.use(express.json());
+server.use(helmet());
+
+//Custom middleware
+server.use(methodLogger);
+server.use(addName);
 
 server.use('/api/hubs', hubsRouter);
 
@@ -16,5 +25,15 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+function methodLogger(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
+
+function addName(req, res, next) {
+  req.name = req.name || 'Eric';
+  next();
+}
 
 module.exports = server;
